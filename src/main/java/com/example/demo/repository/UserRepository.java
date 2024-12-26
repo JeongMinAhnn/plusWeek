5b70aace050c43443dbc9b657c0fc2f914c26300
JeongMinAhnn <133354826+JeongMinAhnn@users.noreply.github.com>
2024-12-26 18:40:55 +0900

    feat : User 필요한 부분만 갱신

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,10 +1,21 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.Item;
 import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
+  @Modifying
+  @Query("UPDATE User u SET u.status = 'PENDING' WHERE u.status = 'APPROVED' AND u.role = 'USER' AND u.id IN :userIds")
+  void updatePendingStatus(List<Long> userIds);
+  default User findUserById(Long id) {
+    return findById(id).orElseThrow(() -> new IllegalArgumentException("해당 ID에 맞는 값이 존재하지 않습니다."));
+  }
 }
