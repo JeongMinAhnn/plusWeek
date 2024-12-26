955a929c60df0675c7466d04b6d8508e6b69de56
JeongMinAhnn <133354826+JeongMinAhnn@users.noreply.github.com>
2024-12-26 18:39:41 +0900

    feat : @Query 코드 간결화

--- src/main/java/com/example/demo/repository/ReservationRepository.java
+++ src/main/java/com/example/demo/repository/ReservationRepository.java
@@ -11,13 +11,10 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+public interface ReservationRepository extends JpaRepository<Reservation, Long>, ReservationRepositoryCustom {
 
-    List<Reservation> findByUserIdAndItemId(Long userId, Long itemId);
-
-    List<Reservation> findByUserId(Long userId);
-
-    List<Reservation> findByItemId(Long itemId);
+    @Query("SELECT r FROM Reservation r JOIN FETCH r.user JOIN FETCH r.item ")
+    List<Reservation> findAllWithUserAndItem();
 
     @Query("SELECT r FROM Reservation r " +
             "WHERE r.item.id = :id " +
