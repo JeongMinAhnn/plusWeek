582809861d9a02af1b2a91fd668db3b25fc10f81
JeongMinAhnn <133354826+JeongMinAhnn@users.noreply.github.com>
2024-12-26 18:40:13 +0900

    feat : 주석 수정

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -1,14 +1,9 @@
 package com.example.demo.config;
 
-import com.example.demo.entity.Role;
-import com.example.demo.filter.AuthFilter;
-import com.example.demo.filter.RoleFilter;
+import com.example.demo.interceptor.AdminRoleInterceptor;
 import com.example.demo.interceptor.AuthInterceptor;
 import com.example.demo.interceptor.UserRoleInterceptor;
-import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -19,10 +14,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     // TODO: 2. 인가에 대한 이해
-    private static final String[] AUTH_REQUIRED_PATH_PATTERNS = {"/users/logout", "/admins/*", "/items/*"};
-    private static final String[] USER_ROLE_REQUIRED_PATH_PATTERNS = {"/reservations/*"};
+//    - 개선
+//    - `/admins` 들어오는 요청은 ADMIN 권한을 만들어서 해당 권한이 아니면 요청할 수 없게 만든다.
+    // interceptor 하위 도메인 전체에 적용 **
+    private static final String[] AUTH_REQUIRED_PATH_PATTERNS = {"/users/logout", "/admins/**", "/items/**","/reservations/**"};
+    private static final String[] ADMIN_ROLE_REQUIRED_PATH_PATTERNS = {"/admins/**"};
+    private static final String[] USER_ROLE_REQUIRED_PATH_PATTERNS = {"/reservations/**"};
 
     private final AuthInterceptor authInterceptor;
+    private final AdminRoleInterceptor adminRoleInterceptor;
     private final UserRoleInterceptor userRoleInterceptor;
 
     @Override
@@ -31,26 +31,12 @@ public class WebConfig implements WebMvcConfigurer {
                 .addPathPatterns(AUTH_REQUIRED_PATH_PATTERNS)
                 .order(Ordered.HIGHEST_PRECEDENCE);
 
+        registry.addInterceptor(adminRoleInterceptor)
+                .addPathPatterns(ADMIN_ROLE_REQUIRED_PATH_PATTERNS)
+                    .order(Ordered.HIGHEST_PRECEDENCE + 1);
+
         registry.addInterceptor(userRoleInterceptor)
                 .addPathPatterns(USER_ROLE_REQUIRED_PATH_PATTERNS)
                 .order(Ordered.HIGHEST_PRECEDENCE + 2);
-    }
-
-    @Bean
-    public FilterRegistrationBean authFilter() {
-        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
-        filterRegistrationBean.setFilter(new AuthFilter());
-        filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        filterRegistrationBean.addUrlPatterns(AUTH_REQUIRED_PATH_PATTERNS);
-        return filterRegistrationBean;
-    }
-
-    @Bean
-    public FilterRegistrationBean userRoleFilter() {
-        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
-        filterRegistrationBean.setFilter(new RoleFilter(Role.USER));
-        filterRegistrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE + 2);
-        filterRegistrationBean.addUrlPatterns(USER_ROLE_REQUIRED_PATH_PATTERNS);
-        return filterRegistrationBean;
     }
 }
--- src/main/java/com/example/demo/service/ReservationService.java
+++ src/main/java/com/example/demo/service/ReservationService.java
@@ -19,111 +19,115 @@ import java.util.Optional;
 
 @Service
 public class ReservationService {
-    private final ReservationRepository reservationRepository;
-    private final ItemRepository itemRepository;
-    private final UserRepository userRepository;
-    private final RentalLogService rentalLogService;
+  private final ReservationRepository reservationRepository;
+  private final ItemRepository itemRepository;
+  private final UserRepository userRepository;
+  private final RentalLogService rentalLogService;
 
-    public ReservationService(ReservationRepository reservationRepository,
-                              ItemRepository itemRepository,
-                              UserRepository userRepository,
-                              RentalLogService rentalLogService) {
-        this.reservationRepository = reservationRepository;
-        this.itemRepository = itemRepository;
-        this.userRepository = userRepository;
-        this.rentalLogService = rentalLogService;
+  public ReservationService(ReservationRepository reservationRepository,
+                            ItemRepository itemRepository,
+                            UserRepository userRepository,
+                            RentalLogService rentalLogService) {
+    this.reservationRepository = reservationRepository;
+    this.itemRepository = itemRepository;
+    this.userRepository = userRepository;
+    this.rentalLogService = rentalLogService;
+  }
+
+  // TODO: 1. 트랜잭션 이해
+//  - 개선
+//    - `createReservation` 함수 내에서
+//        - 하나라도 에러가 발생하면 모두 저장되지 않도록 수정합니다.
+//        - 하나라도 에러가 발생하지 않으면 모두 저장되도록 수정합니다.
+//        - All or Nothing
+  @Transactional
+  public void createReservation(Long itemId, Long userId, LocalDateTime startAt, LocalDateTime endAt) {
+    // 쉽게 데이터를 생성하려면 아래 유효성검사 주석 처리
+    List<Reservation> haveReservations = reservationRepository.findConflictingReservations(itemId, startAt, endAt);
+    if (!haveReservations.isEmpty()) {
+      throw new ReservationConflictException("해당 물건은 이미 그 시간에 예약이 있습니다.");
     }
+    //repository 코드 분리
+    Item item = itemRepository.findItemById(itemId);
+    User user = userRepository.findUserById(userId);
+    Reservation reservation = new Reservation(item, user, "PENDING", startAt, endAt);
+    Reservation savedReservation = reservationRepository.save(reservation);
 
-    // TODO: 1. 트랜잭션 이해
-    public void createReservation(Long itemId, Long userId, LocalDateTime startAt, LocalDateTime endAt) {
-        // 쉽게 데이터를 생성하려면 아래 유효성검사 주석 처리
-        List<Reservation> haveReservations = reservationRepository.findConflictingReservations(itemId, startAt, endAt);
-        if(!haveReservations.isEmpty()) {
-            throw new ReservationConflictException("해당 물건은 이미 그 시간에 예약이 있습니다.");
-        }
+    RentalLog rentalLog = new RentalLog(savedReservation, "로그 메세지", "CREATE");
+    rentalLogService.save(rentalLog);
+  }
 
-        Item item = itemRepository.findById(itemId).orElseThrow(() -> new IllegalArgumentException("해당 ID에 맞는 값이 존재하지 않습니다."));
-        User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("해당 ID에 맞는 값이 존재하지 않습니다."));
-        Reservation reservation = new Reservation(item, user, "PENDING", startAt, endAt);
-        Reservation savedReservation = reservationRepository.save(reservation);
+  // TODO: 3. N+1 문제
+//  - 개선
+//    - 동일한 데이터를 가져올 때 N+1 문제가 발생하지 않게 수정합니다.
+  public List<ReservationResponseDto> getReservations() {
+    List<Reservation> reservations = reservationRepository.findAllWithUserAndItem();
 
-        RentalLog rentalLog = new RentalLog(savedReservation, "로그 메세지", "CREATE");
-        rentalLogService.save(rentalLog);
+    return reservations.stream().map(reservation ->
+
+        new ReservationResponseDto(
+            reservation.getId(),
+            reservation.getUser().getNickname(),
+            reservation.getItem().getName(),
+            reservation.getStartAt(),
+            reservation.getEndAt()
+        )).toList();
+  }
+
+  /*
+  implementation "com.querydsl:querydsl-jpa:5.0.0:jakarta"
+  annotationProcessor "com.querydsl:querydsl-apt:5.0.0:jakarta"
+  annotationProcessor "jakarta.annotation:jakarta.annotation-api"
+  annotationProcessor "jakarta.persistence:jakarta.persistence-api"
+  build.gradle dependencies 추가
+  */
+  // TODO: 5. QueryDSL 검색 개선
+//  - 개선
+//    - `QueryDSL`을 활용하여 동적 쿼리를 적용합니다.
+//    - N+1 문제가 발생하지 않도록 합니다.
+  public List<ReservationResponseDto> searchAndConvertReservations(Long userId, Long itemId) {
+
+    List<Reservation> reservations = reservationRepository.searchReservations(userId, itemId);
+
+    return convertToDto(reservations);
+  }
+
+
+  private List<ReservationResponseDto> convertToDto(List<Reservation> reservations) {
+    return reservations.stream()
+        .map(reservation -> new ReservationResponseDto(
+            reservation.getId(),
+            reservation.getUser().getNickname(),
+            reservation.getItem().getName(),
+            reservation.getStartAt(),
+            reservation.getEndAt()
+        ))
+        .toList();
+  }
+
+  // TODO: 7. 리팩토링
+//  - 개선
+//    1. 필요하지 않은 `else` 구문을 걷어냅니다.
+//      2. 컨트롤러 응답 데이터 타입을 적절하게 변경합니다.
+//      3. 재사용 비중이 높은 `findById` 함수들을 `default` 메소드로 선언합니다.
+//      4. 상태 값을 명확하게 `enum`으로 관리합니다.
+//      5. 첫번째 Transactional 문제를 해결했다면 `RentalLogService` save 함수 내 19~21번째 코드를 삭제하거나 주석처리하여 기능이 동작하도록 수정합니다.
+  @Transactional
+  public void updateReservationStatus(Long reservationId, String status) {
+    Reservation reservation = reservationRepository.findById(reservationId).orElseThrow(() -> new IllegalArgumentException("해당 ID에 맞는 데이터가 존재하지 않습니다."));
+
+    if ("APPROVED".equals(status) && !"PENDING".equals(reservation.getStatus())) {
+      throw new IllegalArgumentException("PENDING 상태만 APPROVED로 변경 가능합니다.");
+
     }
+    if ("CANCELED".equals(status) && "EXPIRED".equals(reservation.getStatus())) {
+      throw new IllegalArgumentException("EXPIRED 상태인 예약은 취소할 수 없습니다.");
 
-    // TODO: 3. N+1 문제
-    public List<ReservationResponseDto> getReservations() {
-        List<Reservation> reservations = reservationRepository.findAll();
-
-        return reservations.stream().map(reservation -> {
-            User user = reservation.getUser();
-            Item item = reservation.getItem();
-
-            return new ReservationResponseDto(
-                    reservation.getId(),
-                    user.getNickname(),
-                    item.getName(),
-                    reservation.getStartAt(),
-                    reservation.getEndAt()
-            );
-        }).toList();
     }
+    if ("EXPIRED".equals(status) && !"PENDING".equals(reservation.getStatus())) {
+      throw new IllegalArgumentException("PENDING 상태만 EXPIRED로 변경 가능합니다.");
 
-    // TODO: 5. QueryDSL 검색 개선
-    public List<ReservationResponseDto> searchAndConvertReservations(Long userId, Long itemId) {
-
-        List<Reservation> reservations = searchReservations(userId, itemId);
-
-        return convertToDto(reservations);
     }
-
-    public List<Reservation> searchReservations(Long userId, Long itemId) {
-
-        if (userId != null && itemId != null) {
-            return reservationRepository.findByUserIdAndItemId(userId, itemId);
-        } else if (userId != null) {
-            return reservationRepository.findByUserId(userId);
-        } else if (itemId != null) {
-            return reservationRepository.findByItemId(itemId);
-        } else {
-            return reservationRepository.findAll();
-        }
-    }
-
-    private List<ReservationResponseDto> convertToDto(List<Reservation> reservations) {
-        return reservations.stream()
-                .map(reservation -> new ReservationResponseDto(
-                        reservation.getId(),
-                        reservation.getUser().getNickname(),
-                        reservation.getItem().getName(),
-                        reservation.getStartAt(),
-                        reservation.getEndAt()
-                ))
-                .toList();
-    }
-
-    // TODO: 7. 리팩토링
-    @Transactional
-    public void updateReservationStatus(Long reservationId, String status) {
-        Reservation reservation = reservationRepository.findById(reservationId).orElseThrow(() -> new IllegalArgumentException("해당 ID에 맞는 데이터가 존재하지 않습니다."));
-
-        if ("APPROVED".equals(status)) {
-            if (!"PENDING".equals(reservation.getStatus())) {
-                throw new IllegalArgumentException("PENDING 상태만 APPROVED로 변경 가능합니다.");
-            }
-            reservation.updateStatus("APPROVED");
-        } else if ("CANCELED".equals(status)) {
-            if ("EXPIRED".equals(reservation.getStatus())) {
-                throw new IllegalArgumentException("EXPIRED 상태인 예약은 취소할 수 없습니다.");
-            }
-            reservation.updateStatus("CANCELED");
-        } else if ("EXPIRED".equals(status)) {
-            if (!"PENDING".equals(reservation.getStatus())) {
-                throw new IllegalArgumentException("PENDING 상태만 EXPIRED로 변경 가능합니다.");
-            }
-            reservation.updateStatus("EXPIRED");
-        } else {
-            throw new IllegalArgumentException("올바르지 않은 상태: " + status);
-        }
-    }
+    reservation.updateStatus(status);
+  }
 }
